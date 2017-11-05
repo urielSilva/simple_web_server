1c4ac5a247eeca5cc1a8116c7d3b370f7fbe9a86
urielSilva <urielbcs@gmail.com>
2017-11-05 15:36:24 -0200

    making sure the clearer executes before readers created after it

--- main.c
+++ main.c
@@ -11,79 +11,89 @@
 #define FILE_NAME "data.txt"
 pthread_mutex_t leitores = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t bd = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t contador = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t vez = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t counter = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t wait_clearer = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t clearer_cond = PTHREAD_COND_INITIALIZER;
 pthread_barrier_t barrier;
 pthread_t workers[10];
 char* db_value[10];
 int num_workers = 0;
-int existe_clearer = 0;
+int clearer_exists = 0;
 
 void* writer(void* msg){
   Message* message = (Message *) msg;
+  pthread_mutex_lock(&wait_clearer);
+  while(clearer_exists) {
+    printf("VOU ESPERAR O CLEARER\n");
+    pthread_cond_wait(&clearer_cond, &wait_clearer);
+  }
+  pthread_mutex_unlock(&wait_clearer);
 	pthread_mutex_lock(&bd);
-  sleep(5);
   printf("escrevendo %s\n", message->body);
   FILE* fp = fopen(FILE_NAME, "a+");
   fprintf(fp, "%s", message->body);
   fclose(fp);
 	sleep(3);
 	pthread_mutex_unlock(&bd);
-  pthread_mutex_lock(&contador);
+  pthread_mutex_lock(&counter);
   num_workers--;
   printf("Finalizou escrita, num workers: %d!\n", num_workers);
-  pthread_mutex_unlock(&contador);
-  if(existe_clearer) pthread_barrier_wait(&barrier);
+  pthread_mutex_unlock(&counter);
+  if(clearer_exists) pthread_barrier_wait(&barrier);
 }
 
 void* reader(void* msg){
   Message* message = (Message *) msg;
+  pthread_mutex_lock(&wait_clearer);
+  while(clearer_exists) {
+    printf("VOU ESPERAR O CLEARER\n");
+    pthread_cond_wait(&clearer_cond, &wait_clearer);
+  }
+  pthread_mutex_unlock(&wait_clearer);
+	pthread_mutex_lock(&bd);
+  FILE* fp = fopen(FILE_NAME, "r");
+  char body[10];
+  fscanf(fp, "%s", body);
+  fclose(fp);
+  printf("Leitura: %s!\n", body);
+  pthread_mutex_unlock(&bd);
   sleep(5);
-		pthread_mutex_lock(&bd);
-    FILE* fp = fopen(FILE_NAME, "r");
-    char body[10];
-    fscanf(fp, "%s", body);
-    fclose(fp);
-    printf("Leitura: %s!\n", body);
-    pthread_mutex_unlock(&bd);
-		sleep(5);
-    pthread_mutex_lock(&contador);
-    num_workers--;
-    printf("Finalizou leitura, num workers: %d!\n", num_workers);
-    pthread_mutex_unlock(&contador);
-    if(existe_clearer) pthread_barrier_wait(&barrier);
+  pthread_mutex_lock(&counter);
+  num_workers--;
+  printf("Finalizou leitura, num workers: %d!\n", num_workers);
+  pthread_mutex_unlock(&counter);
+  if(clearer_exists) pthread_barrier_wait(&barrier);
 }
 
 void* clearer(void* msg){
   pthread_barrier_init(&barrier, NULL, num_workers);
   printf("VOU PARAR NA BARREIRA COM %d\n", num_workers);
+  sleep(4);
   pthread_barrier_wait(&barrier);
   printf("Limpando o arquivo.\n");
   FILE* fp = fopen(FILE_NAME, "w");
   fclose(fp);
-  pthread_mutex_lock(&contador);
+  pthread_mutex_lock(&counter);
   printf("SAI DA BARREIRA\n");
   num_workers--;
-  existe_clearer = 0;
-  pthread_mutex_unlock(&contador);
-  sleep(2);
+  clearer_exists = 0;
+  pthread_mutex_unlock(&counter);
+  printf("LIBERANDO A GALERA\n");
+  pthread_cond_broadcast(&clearer_cond);
+
 }
 
 void create_worker(Message* message) {
   message->id = num_workers;
   int n;
   if(strcmp(message->command, "write") == 0) {
-    printf("criei um writer\n");
     n = pthread_create(&(workers[num_workers]), NULL, writer,(void *) message);
   } else if(strcmp(message->command, "read") == 0) {
-    printf("criei um reader\n");
     n = pthread_create(&(workers[num_workers]), NULL, reader,(void *) message);
   } else if(strcmp(message->command, "clear") == 0) {
-    existe_clearer = 1;
-    printf("criei um clear\n");
+    clearer_exists = 1;
     n = pthread_create(&(workers[num_workers]), NULL, clearer,(void *) message);
   }
-  printf("erro: %d\n", n);
   num_workers++;
 }
 
